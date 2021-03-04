1da3439d5518f9e1de90b55622946dca1c0403cb
Ivan Yosifov <ivan_yosifov@yahoo.com>
2021-03-04 13:55:46 +0200

    create form & controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -35,7 +35,17 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validate the input
+        $request->validate([
+            'name' => 'required',
+            'details' => 'required'
+        ]);
+
+        // create a new product in the database
+        Product::create($request->all());
+
+        // redirect the user and send friendly message
+        return redirect()->route('products.index')->with('success', 'Product created successfully');
     }
 
     /**
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,5 +1,44 @@
 @extends('products.layout')
 
 @section('content')
-<h1>Create Product</h1>
+<div class="row">
+	<div class="col-lg-12 margin-tb">
+		<div class="d-flex justify-content-between align-items-center">
+			<h2>Add New Product</h2>
+			<a href="{{ route('products.index') }}" class="btn btn-primary">Back</a>
+		</div>		
+	</div>
+</div>
+
+@if ($errors->any())
+<div class="alert alert-danger">
+	<strong>Whoops!</strong> There were some problems with your input. <br><br>
+	<ul>
+		@foreach($errors->all() as $error)
+		<li>{{ $error }}</li>
+		@endforeach
+	</ul>
+</div>
+@endif
+
+<form action="{{ route('products.store') }}" method="POST">
+	@csrf
+	<div class="row">
+		<div class="col-12">
+			<div class="form-group">
+				<label for="name">Name</label>
+				<input type="text" name="name" id="name" class="form-control" placeholder="Enter product name">
+			</div>
+			<div class="col-12">
+				<div class="form-group">
+					<label for="details">Details</label>
+					<textarea class="form-control" name="details" id="details" rows="5" placeholder="Enter product details"></textarea>
+				</div>
+			</div>
+			<div class="col-12 text-center mt-3">
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</div>
+		</div>
+	</div>
+</form>
 @endsection
